b90f842acb575305bef3e8a5bd7e553ee8abc282
Aman Garg <amangarg1995sep@gmail.com>
2022-11-27 18:33:10 +0530

    Use bitsstdc++.h (not for professional projects)

--- code-cpp/arraySplit.cpp
+++ code-cpp/arraySplit.cpp
@@ -1,4 +1,5 @@
-#include<bits/stdc++.h>
+#include "bitsstdc++.h"
+
 using namespace std;
 
 typedef long long ll;
@@ -6,7 +7,7 @@ typedef long long ll;
 ll a[100010];
 
 ll sum(int l, int r) {
-    return a[r] - a[l-1];
+    return a[r] - a[l - 1];
 }
 
 // binary search to find first valid X
@@ -15,12 +16,12 @@ int get(int l, int r, ll s) {
     int low, high, mid;
     low = l;
     high = r;
-    while( low <= high ) {
-        mid = ( low + high ) / 2;
+    while (low <= high) {
+        mid = (low + high) / 2;
         ll x = sum(l, mid);
-        if( x == s && (mid == l || sum(l, mid-1) != s )) {
+        if (x == s && (mid == l || sum(l, mid - 1) != s)) {
             return mid;
-        } else if( x >= s ) {
+        } else if (x >= s) {
             high = mid - 1;
         } else {
             low = mid + 1;
@@ -29,13 +30,13 @@ int get(int l, int r, ll s) {
     return -1;
 }
 
-int solve(int l,int r){
+int solve(int l, int r) {
     ll s = sum(l, r);
-    if( l != r && s % 2 == 0 ) {
-        int ind = get(l, r, s/2);
-        if( ind != -1 ) {
+    if (l != r && s % 2 == 0) {
+        int ind = get(l, r, s / 2);
+        if (ind != -1) {
             // compute maximum from 2 parts
-            return max(solve(l, ind), solve(ind+1, r)) + 1;
+            return max(solve(l, ind), solve(ind + 1, r)) + 1;
         }
     }
     return 0;
@@ -43,16 +44,16 @@ int solve(int l,int r){
 
 int main() {
     int t;
-    cin>>t;
-    while (t--){
+    cin >> t;
+    while (t--) {
         int n;
-        cin>>n;
+        cin >> n;
         assert(n <= 16384);
-        for (int i=1;i<=n;i++) {
-            cin>>a[i];
-            a[i]+=a[i-1];
+        for (int i = 1; i <= n; i++) {
+            cin >> a[i];
+            a[i] += a[i - 1];
         }
-        cout<<solve(1,n)<<endl;
+        cout << solve(1, n) << endl;
     }
     return 0;
 }
